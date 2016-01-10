6c70382957e89dd153c18e3140f2f1243b94ab82
kelostrada <kelostrada@gmail.com>
2016-01-10 14:52:05 -0500

    finished Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,21 @@ class PostsController < ApplicationController
   end
   
   def new
+    @new_post = Post.new
   end
 
   def create
+    post = Post.create :content => params[:post][:content], :user_id => params[:post][:user_id]
+    
+    if post.valid?
+      flash[:success] = ["Post created successfully"]
+    else
+      flash[:error] = post.errors.full_messages
+    end
+    
+    flash.keep
+    
+    redirect_to :action => :index
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  validates :content, presence: true
+  validates :user_id, presence: true
+  
+  def author
+    user.name
+  end
 end
